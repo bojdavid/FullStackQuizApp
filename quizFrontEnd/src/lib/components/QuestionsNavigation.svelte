39b3bea38e820880b6e38d8df18e8021c9caa341
bojdavid <bojdavid80@gmail.com>
2025-03-16 23:11:12 +0100

    sleepy commit

--- quizFrontEnd/src/lib/components/QuestionsNavigation.svelte
+++ quizFrontEnd/src/lib/components/QuestionsNavigation.svelte
@@ -1,19 +1,67 @@
 <script>
-    let {questions} = $props();
+    import { Tooltip } from '@skeletonlabs/skeleton-svelte';
+    let openState = $state(false);
 
-    let questionCount = 0;
-    let answeredStyle = 'bg-secondary'
+    let {questions, questionNum, goToQuestion} = $props();
+    
+    let answeredQuestions = $state([]);
+    //let ansq  = $derived(questions.map(q => ({ isAnswered: q.answered, id: q.id })));
+        
+
+    // Ensure `answeredQuestions` updates whenever `questions` change
+    $effect(() => {
+        answeredQuestions = questions.map(q => ({ isAnswered: q.answered, id: q.id }));
+        //console.log($state.snapshot(("Updated answeredQuestions:", an sweredQuestions)));
+    });
+   
+
+    let isViewNavs = $state(false); 
+const viewQuestionNavs = ()=>{
+    answeredQuestions = questions.map(q => ({ isAnswered: q.answered, id: q.id }));
+    isViewNavs = !isViewNavs;
+}
+
+
+ 
 </script>
 
-<div class="flex justify-start flex-wrap">
-    {#each questions as question}
-        <div class="rounded-full w-15 h-15 text-lg font-bold bg-primary-500 text-center m-2 
-                    flex items-center justify-center" class:answeredStyle={question.answered}>
-            {questionCount += 1}
+
+<Tooltip
+  open={openState}
+  onOpenChange={(e) => (openState = e.open)}
+  positioning={{ placement: 'right' }}
+  triggerBase="underline"
+  contentBase="card preset-filled p-2"
+  openDelay={200}
+  arrow
+>
+    {#snippet trigger()}
+        <div style="display: contents">
+            <button onclick={viewQuestionNavs} class="btn preset-filled-primary-500">
+                Question <span>{questionNum + 1}</span>
+            </button>
         </div>
-    {/each}
+    {/snippet}
+  {#snippet content()}Click to view the questions you have answered.{/snippet}
+</Tooltip>
+
+<div class="flex justify-start flex-wrap">
+    <!--    WHEN USER ANSWERS A QUESTION, THE BACKGROUND CHANGES-->
+    <!--    WHEN USER CLICKS ON A QUESTION THEY NAVIGATE TO THE QUESTION THEY CLICK ON-->
+    {#if isViewNavs}
+        {#each answeredQuestions as question}
+            <button class="rounded-full w-13 h-13 text-lg font-bold {question.isAnswered? "bg-primary-500" : "bg-secondary-500"} text-center m-2 
+                        flex items-center justify-center"
+                        onclick={() => goToQuestion(question.id)}
+                        class:active={questionNum == question.id}>
+                {question.id + 1}
+            </button>
+        {/each}
+    {/if}
 </div>
 
 <style>
-
+ .active{
+      background-color: red;
+  }
 </style>
---
+++ quizFrontEnd/src/routes/quiz/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+import "../../styles/app.css";
+import QuizContainer from "$lib/components/QuizContainer.svelte";
+import QuestionsNavigation from "$lib/components/QuestionsNavigation.svelte";
+
+let { data } = $props();
+let questions = data.questions
+let questionNum = $state(0);
+let viewCorrect = $state(false);
+let selectedAns = $state(true);
+let hideItem = "hidden";
+
+
+//Add a property for when the question has been answered
+for(let i = 0; i < questions.length; i++){
+    questions[i].answered = false;
+    questions[i].id = i
+    // console.log(questions[i])
+}
+
+
+const goToNextQuestion = () => {
+    questionNum++;
+    viewCorrect = false;
+    }
+
+const viewCorrectAns = () => {
+    viewCorrect=true;
+}
+
+const goToQuestion = (id) =>{
+    console.log(id)
+    questionNum = id;
+}
+
+</script>
+
+<main class="mx-auto min-h-screen flex flex-col items-center justify-center ">
+    <div class="min-w-xs w-full max-w-2xl mx-2 px-2">
+        <QuizContainer question = {questions[questionNum]} {viewCorrect} {goToNextQuestion} {selectedAns}/>
+
+        
+        <section class="mb-10">
+            <div class="w-full flex justify-start mt-10" >
+                <button type="button" class="btn preset-filled-secondary-500" onclick={viewCorrectAns}>
+                    {#if  !viewCorrect}
+                        View Correct Answer
+                    {:else}
+                        View Reason for Answer
+                    {/if}
+                </button>
+            </div>
+    
+            <!--
+            <div class:hidden={!viewCorrect}>
+                {questions[questionNum].CorrectOption}
+            </div>
+            -->
+        </section>
+
+        <QuestionsNavigation  {questions} {questionNum} {goToQuestion}/>
+    </div>
+        
+</main>
+
+<style>
+
+
+</style>
